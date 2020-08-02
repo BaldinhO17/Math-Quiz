c12c5722d795410243ab88d129685c04d404298b
Eribaldo <eribaldo.barros@escolar.ifrn.edu.br>
2020-08-02 20:15:32 -0300

    Criando as apps

--- Backend/Math_Quiz/settings.py
+++ Backend/Math_Quiz/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'Math_Quiz.user',
+    'Math_Quiz.answer',
+    'Math_Quiz.question',
+    'Math_Quiz.alternative',
+    
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
