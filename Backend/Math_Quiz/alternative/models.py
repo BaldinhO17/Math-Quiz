600844e90ac866b8e5025b8b59f7e5a48187340d
Eribaldo <eribaldo.barros@escolar.ifrn.edu.br>
2020-08-03 20:05:58 -0300

    Disponibilizandi primeiro modelo da app Alternative

--- Backend/Math_Quiz/alternative/models.py
+++ Backend/Math_Quiz/alternative/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Alternative(models.Model):
+    text = models.TextField()
+    isCorrect = models.BooleanField()
+
+    def __str__(self):
+        return self.text
--- Backend/Math_Quiz/urls.py
+++ Backend/Math_Quiz/urls.py
@@ -1,9 +1,4 @@
-"""Math_Quiz URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
+"""
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
@@ -13,9 +8,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
 from django.contrib import admin
-from django.urls import path
+from Math_Quiz.alternative.api.viewsets import AlternativeViewSet
+
+
+router = DefaultRouter()
+router.register(r'alternative', AlternativeViewSet)
+
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
